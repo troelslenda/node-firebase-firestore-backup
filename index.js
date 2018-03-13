112724049450f8a8c95ac0f2b058aa7ddf25c680
Troels Lenda <troelslenda@gmail.com>
2018-03-13 15:04:44 +0100

    updating

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const commander = require('commander')
 
 const admin = require('firebase-admin')
+const fs = require('fs')
 
 const backup = require('./backup')
 const restore = require('./restore')
@@ -11,13 +12,21 @@ if (!process.argv.slice(2).length) {
   process.exit(0)
 }
 
+
 const handleCmd = cmd => {
   if (!cmd.parent.credentialsFile) {
     console.log('You have to specify a credentials file using the -C option.')
     process.exit(-1)
   }
 
-  let serviceAccount = require(cmd.parent.credentialsFile)
+  let serviceAccount = ''
+  //let serviceAccount = require(cmd.parent.credentialsFile)
+  if (cmd.parent.credentialsString) {
+    serviceAccount = cmd.parent.credentialsString
+  } else {
+    const serviceAccountBuffer = fs.readFileSync(cmd.parent.credentialsFile)
+    serviceAccount = JSON.parse(serviceAccountBuffer.toString())
+  }
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   })
@@ -25,6 +34,7 @@ const handleCmd = cmd => {
 }
 
 commander
+  .option('-x, --credentials-string <string>', 'The credentials as string. Trumps --credentials-file.')
   .option('-C, --credentials-file <file>', 'The credentials file to use.')
   .option('-B, --bucket-name <string>', 'The name of the bucket to upload backups')
   .option('-o, --only [collections...]', 'Only use these collections. Trumps --except.')
